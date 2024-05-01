036a282c77369386b44a3f4a4d9f02e5eb9de733
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-05-01 10:33:40 +0100

    mvc architecture

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 // Load environment variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
