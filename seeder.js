549b7496466a5cf56fc61f6431a64e6efa00f03d
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-07-23 23:41:30 +0100

    added seeder script and course route

--- seeder.js
+++ seeder.js
@@ -8,19 +8,25 @@ dotenv.config({ path: "./config/config.env" });
 
 // Load Models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
-// Connect to DB
+// Connect to DB - Loading MONGO_URI from environment variable
 mongoose.connect(process.env.MONGO_URI);
 
-// Read JSON files
+// Read JSON files - JSON.parse() converts the data to a JSON object
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
-// Import into DB
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
+// Import into DB - function
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
 
     console.log("Data Imported...".green.bold.inverse);
     process.exit();
@@ -33,6 +39,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
 
     console.log("Data Destroyed...".red.bold.inverse);
     process.exit();
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ connectDB();
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
 
 const app = express();
 
@@ -26,16 +27,21 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
 
 // Custom Error Handler
+// Must be under the Mount Routers, otherwise it won't work...
 app.use(errorHandler);
 
+// PORT
 const PORT = process.env.PORT || 5000;
 
+// Root route
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, data: { id: 1 } });
 });
 
+// Server listening on PORT
 const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
